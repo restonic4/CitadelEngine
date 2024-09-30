32eaf678f04e2d4ccbd44ced8bf53049dd55b88f
restonic4 <marcosruizlerma@gmail.com>
2024-09-30 16:44:06 +0200

    Cleanup

--- src/main/java/com/restonic4/test/TestClient.java
+++ src/main/java/com/restonic4/test/TestClient.java
@@ -1,11 +1,12 @@
 package com.restonic4.test;
 
 import com.restonic4.citadel.core.GameLogic;
+import com.restonic4.citadel.world.SceneManager;
 
 public class TestClient implements GameLogic {
     @Override
     public void start() {
-
+        SceneManager.loadScene(new TestScene());
     }
 
     @Override
