5a1aa37af3a727f4940e7b400b87d4a501048001
restonic4 <marcosruizlerma@gmail.com>
2024-10-02 13:02:35 +0200

    Cleanup

--- src/main/java/com/restonic4/citadel/render/gui/guis/editor/EditorInspectorImGui.java
+++ src/main/java/com/restonic4/citadel/render/gui/guis/editor/EditorInspectorImGui.java
@@ -10,6 +10,7 @@ import com.restonic4.citadel.world.Scene;
 import com.restonic4.citadel.world.SceneManager;
 import com.restonic4.citadel.world.object.GameObject;
 import imgui.ImGui;
+import imgui.ImVec2;
 import imgui.flag.ImGuiTreeNodeFlags;
 
 public class EditorInspectorImGui extends ToggleableImGuiScreen {
